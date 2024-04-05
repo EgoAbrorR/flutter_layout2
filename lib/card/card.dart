d1c20149e296f7dceff8a192a8aed19ca5c44273
EgoAbror <rahsando@gmail.com>
2024-04-05 23:40:47 +0700

    first commit

---
+++ lib/card/card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const GridCount());
+}
+
+class GridCount extends StatelessWidget {
+  const GridCount({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Grid Count"),
+          ),
+          body: const Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Ego Abror R'),
+                  subtitle: Text('Newbie'),
+                ),
+                Text(
+                  'Tekmob',
+                  style: TextStyle(fontSize: 40),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
